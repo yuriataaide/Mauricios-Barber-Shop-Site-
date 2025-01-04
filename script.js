b67f9fe11bb4408c709b98dc4c39eea928e14cd5
Yuri Ataide <yuriizinho@outlook.com>
2025-01-04 13:00:43 -0300

    JavaScript functions and general fixes

---
+++ script.js
@@ -0,0 +1,14 @@
+const isaiasButton = document.getElementById('isaias')
+    isaiasButton.addEventListener('click', () => {
+        window.open('https://isaias.youcanbook.me/', '_blank')
+    })
+
+const mauricioButton = document.getElementById('mauricio')
+    mauricioButton.addEventListener('click', () => {
+        window.open('https://mauriciojoaorosa-filho.youcanbook.me/', '_blank')
+    })
+
+const eliasButton = document.getElementById('elias')
+    eliasButton.addEventListener('click', () => {
+        window.open('https://eliasmartins.youcanbook.me/', '_blank')
+    })
